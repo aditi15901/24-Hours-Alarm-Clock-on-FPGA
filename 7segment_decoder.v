e258e68f6fa4ca8e8cb95b747e540e75f4404c5f
Aditi Das <93090825+aditi15901@users.noreply.github.com>
2021-11-07 19:03:50 +0530

    Update 7segment_decoder.v

--- 7segment_decoder.v
+++ 7segment_decoder.v
@@ -10,15 +10,15 @@ always @(in)
 begin
   case(in)
   0: display= 7'b0000001; //displays zero on the seven segment
-  1: display= 7'b1001111; //displays 1
-  2: display= 7'b0010010; //displays 2
-  3: display= 7'b0000110; //displays 3
-  4: display= 7'b1001100; //displays 4
-  5: display= 7'b0100100; //displays 5
-  6: display= 7'b0100000; //displays 6
-  7: display= 7'b0001111; //displays 7
-  8: display= 7'b0000000; //displays 8
-  9: display= 7'b0000100; //displays 9
+  1: display= 7'b1001111; //displays 1 on the seven segment
+  2: display= 7'b0010010; //displays 2 on the seven segment
+  3: display= 7'b0000110; //displays 3 on the seven segment
+  4: display= 7'b1001100; //displays 4 on the seven segment
+  5: display= 7'b0100100; //displays 5 on the seven segment
+  6: display= 7'b0100000; //displays 6 on the seven segment
+  7: display= 7'b0001111; //displays 7 on the seven segment
+  8: display= 7'b0000000; //displays 8 on the seven segment
+  9: display= 7'b0000100; //displays 9 on the seven segment
   endcase
 end
 
